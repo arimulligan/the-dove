dd31680e1c64b29ea3dc590c668317c83cccc9e6
Ari <ariannatnz@gmail.com>
2024-08-02 13:26:31 +1200

    removed and cleaned up stuff

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ async function fetchData() {
     document.getElementById("Value").innerHTML = record.value[0].Value;
 }
 fetchData();
+
+function addOverlayImage(src, opacity) {
+    const img = new Image();
+    img.src = src;
+    Object.assign(img.style, {
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        width: '100vw',
+        height: '100vh',
+        opacity,
+        objectFit: 'cover',
+        objectPosition: 'center center',
+        pointerEvents: 'none'
+    });
+    document.body.appendChild(img);
+}
+
+addOverlayImage('images/download.jpg', 0.5);
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+chrome.scripting
+    .executeScript({
+        target : {tabId : getCurrentTab()},
+        files : [ "script.js" ],
+    })
+    .then(() => console.log("injected script file"));
